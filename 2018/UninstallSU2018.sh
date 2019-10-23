4ef7e3713746276d18dcbc23597d45c2464d0deb
Anton Bursch <antonphd@gmail.com>
2019-10-22 17:21:12 -0700

    initial commit

---
+++ 2018/UninstallSU2018.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#  UninstallPodiumExtrasMac.sh
+#  
+#
+#  Created by Solymosi Robert on 10/10/17.
+#
+
+sudo rm -f /Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/PodiumExtras.rb
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/PodiumExtras
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/PodiumHDR
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/PodiumTemp
+
+sudo rm -f /Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/SU_Podium_V26.rb
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/SU_Podium_V26
+sudo rm -f /Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/SU_Podium_V25.rb
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/SU_Podium_V25
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2018/SketchUp/Styles/Podium
+
+
+sudo rm -f $HOME/Library/Application\ Support/SketchUp\ 2018/SketchUp/Plugins/PodiumExtras.rb
+sudo rm -r -f $HOME/Library/Application\ Support/SketchUp\ 2018/SketchUp/Plugins/PodiumExtras
+sudo rm -r -f $HOME/Library/Application\ Support/SketchUp\ 2018/SketchUp/Plugins/PodiumHDR
+sudo rm -r -f $HOME/Library/Application\ Support/SketchUp\ 2018/SketchUp/Plugins/PodiumTemp
+
+sudo rm -f $HOME/Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/SU_Podium_V26.rb
+sudo rm -rf $HOME/Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/SU_Podium_V26
+sudo rm -f $HOME/Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/SU_Podium_V25.rb
+sudo rm -rf $HOME/Library/Application\ Support/SketchUp\ 2018/SketchUp/plugins/SU_Podium_V25
+sudo rm -rf $HOME/Library/Application\ Support/SketchUp\ 2018/SketchUp/Styles/Podium
