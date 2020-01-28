3deed6d8f61d417dde679cbb4df680ded28b6ac1
Anton Bursch <antonphd@gmail.com>
2020-01-27 20:37:40 -0800

    update 2020 scripts

--- 2020/script/postinstall_SU8.sh
+++ 2020/script/postinstall_SU8.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-chmod 777 "/Library/Application Support/SketchUp 2019/SketchUp/plugins/SU_Podium_V26/SU_Podium_V26/presets/"
-cd "/Library/Application Support/SketchUp 2019/SketchUp/plugins/SU_Podium_V26/SU_Podium_V26/presets/"
+chmod 777 "/Library/Application Support/SketchUp 2020/SketchUp/plugins/SU_Podium_V26/SU_Podium_V26/presets/"
+cd "/Library/Application Support/SketchUp 2020/SketchUp/plugins/SU_Podium_V26/SU_Podium_V26/presets/"
 chmod -R 555 *
-cp -R "/Library/Application Support/SketchUp 2019/SketchUp/plugins/SU_Podium_V26/SU_Podium_V26/Podium" "/Library/Application Support/SketchUp 2019/SketchUp/Styles"
+cp -R "/Library/Application Support/SketchUp 2020/SketchUp/plugins/SU_Podium_V26/SU_Podium_V26/Podium" "/Library/Application Support/SketchUp 2020/SketchUp/Styles"
 
-chmod 777 "/Library/Application Support/SketchUp 2019/SketchUp/plugins"
-if [ -f "/Library/Application Support/SketchUp 2019/SketchUp/plugins/PodiumLicense"]
+chmod 777 "/Library/Application Support/SketchUp 2020/SketchUp/plugins"
+if [ -f "/Library/Application Support/SketchUp 2020/SketchUp/plugins/PodiumLicense"]
 then
-    chmod 777 "/Library/Application Support/SketchUp 2019/SketchUp/plugins/PodiumLicense"
+    chmod 777 "/Library/Application Support/SketchUp 2020/SketchUp/plugins/PodiumLicense"
 fi
--- 2020/script/pre_install_read_license_SU8.sh
+++ 2020/script/pre_install_read_license_SU8.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-sudo rm -f /Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/PodiumExtras.rb
-sudo rm -rf /Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/PodiumExtras
-sudo rm -rf /Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/PodiumHDR
-sudo rm -rf /Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/PodiumTemp
+sudo rm -f /Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/PodiumExtras.rb
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/PodiumExtras
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/PodiumHDR
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/PodiumTemp
 
-sudo rm -f /Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/SU_Podium_V26.rb
-sudo rm -rf /Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/SU_Podium_V26
-sudo rm -f /Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/SU_Podium_V25.rb
-sudo rm -rf /Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/SU_Podium_V25
-sudo rm -rf /Library/Application\ Support/SketchUp\ 2019/SketchUp/Styles/Podium
+sudo rm -f /Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/SU_Podium_V26.rb
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/SU_Podium_V26
+sudo rm -f /Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/SU_Podium_V25.rb
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/SU_Podium_V25
+sudo rm -rf /Library/Application\ Support/SketchUp\ 2020/SketchUp/Styles/Podium
 
 
-sudo rm -f $HOME/Library/Application\ Support/SketchUp\ 2019/SketchUp/Plugins/PodiumExtras.rb
-sudo rm -r -f $HOME/Library/Application\ Support/SketchUp\ 2019/SketchUp/Plugins/PodiumExtras
-sudo rm -r -f $HOME/Library/Application\ Support/SketchUp\ 2019/SketchUp/Plugins/PodiumHDR
-sudo rm -r -f $HOME/Library/Application\ Support/SketchUp\ 2019/SketchUp/Plugins/PodiumTemp
+sudo rm -f $HOME/Library/Application\ Support/SketchUp\ 2020/SketchUp/Plugins/PodiumExtras.rb
+sudo rm -r -f $HOME/Library/Application\ Support/SketchUp\ 2020/SketchUp/Plugins/PodiumExtras
+sudo rm -r -f $HOME/Library/Application\ Support/SketchUp\ 2020/SketchUp/Plugins/PodiumHDR
+sudo rm -r -f $HOME/Library/Application\ Support/SketchUp\ 2020/SketchUp/Plugins/PodiumTemp
 
-sudo rm -f $HOME/Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/SU_Podium_V26.rb
-sudo rm -rf $HOME/Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/SU_Podium_V26
-sudo rm -f $HOME/Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/SU_Podium_V25.rb
-sudo rm -rf $HOME/Library/Application\ Support/SketchUp\ 2019/SketchUp/plugins/SU_Podium_V25
-sudo rm -rf $HOME/Library/Application\ Support/SketchUp\ 2019/SketchUp/Styles/Podium
+sudo rm -f $HOME/Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/SU_Podium_V26.rb
+sudo rm -rf $HOME/Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/SU_Podium_V26
+sudo rm -f $HOME/Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/SU_Podium_V25.rb
+sudo rm -rf $HOME/Library/Application\ Support/SketchUp\ 2020/SketchUp/plugins/SU_Podium_V25
+sudo rm -rf $HOME/Library/Application\ Support/SketchUp\ 2020/SketchUp/Styles/Podium
